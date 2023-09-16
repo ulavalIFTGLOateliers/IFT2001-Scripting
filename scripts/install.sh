c268ac86002ffa39d51f1ec337d3d75782719b15
William Guimont-Martin <willGuimont@users.noreply.github.com>
2023-09-16 14:53:24 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,3 @@
 sudo apt update; sudo apt install -y python3-pip git curl vim debootstrap
 pip3 install "fastapi[all]" "uvicorn[standard]"
+git clone https://github.com/willGuimont/ateliers-iftglo-2001-server ~/server
